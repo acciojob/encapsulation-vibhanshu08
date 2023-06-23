deeebeb9ccf6df52d687f613df764dd04141b7ce
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-06-23 08:07:34 +0530

    Update RWOnly.java
    c

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public void settname(String n){
+        this.name=n;
+    }
+    public String getname(){
+        return this.name;
+    }
 }
