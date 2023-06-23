d15c4ee540cd6f4cfe7385e26324e7c51ccea5a4
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-06-23 08:08:25 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+
 public class Main {
-  
+  public static void main(String args []){
+      
+      RWOnly rwo=new RWOnly();
+//      rwo.name="vibhan";
+      rwo.settname("viban");
+      System.out.println(rwo.getname());
+  }
 }
